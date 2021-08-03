e5902562d90ce5928105f605efa100aa0a37b389
Isana Mizuma <ismizu@hotmail.com>
2021-08-03 19:38:42 -0400

    plots

--- widget.py
+++ widget.py
@@ -3,8 +3,8 @@ import base64
 #import numpy as np
 import streamlit as st
 from datetime import date
-#import plotly.graph_objects as go
-#from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 st.set_page_config(layout = 'wide', initial_sidebar_state = 'collapsed')
 
@@ -119,7 +119,7 @@ def widget_fig(state_value):
 
     return fig
 
-st.plotly_chart('NK_test', use_container_width = True)
+st.plotly_chart('NJ_test.pickle', use_container_width = True)
 
 def component_plot_path(state_value):
 
